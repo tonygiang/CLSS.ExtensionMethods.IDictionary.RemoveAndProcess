e9d8b3a0a213f3d26fa1efa28dd7acfd4189d488
tonygiang <tony_s_giang@yahoo.com>
2022-08-31 01:05:26 +0700

    Project push.

---
+++ IDictionary.RemoveAndProcess.cs
@@ -0,0 +1,92 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System;
+using System.Collections.Generic;
+
+namespace CLSS
+{
+  public static partial class IDictionaryRemoveAndProcess
+  {
+    /// <inheritdoc cref="RemoveAndProcess{T, TKey, TValue, TResult}(T, TKey, Func{TValue, TResult})"/>
+    public static T RemoveAndProcess<T, TKey, TValue>(this T source,
+      TKey key,
+      Action<TValue> processingAction)
+      where T : IDictionary<TKey, TValue>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (processingAction == null)
+        throw new ArgumentNullException("processingAction");
+      var element = source[key];
+      source.Remove(key);
+      processingAction(element);
+      return source;
+    }
+
+    /// <inheritdoc cref="RemoveAndProcess{T, TKey, TValue, TResult}(T, TKey, Func{TValue, TResult})"/>
+    public static IDictionary<TKey, TValue> RemoveAndProcess<TKey, TValue>(
+      this IDictionary<TKey, TValue> source,
+      TKey key,
+      Action<TValue> processingAction)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (processingAction == null)
+        throw new ArgumentNullException("processingAction");
+      var element = source[key];
+      source.Remove(key);
+      processingAction(element);
+      return source;
+    }
+
+    /// <summary>
+    /// Takes the element associated with the specified key and passes it to the
+    /// specified delegate. Removes that element's key from the source
+    /// collection afterward.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="IDictionary{TKey, TValue}"/>
+    /// to remove element from.</typeparam>
+    /// <typeparam name="TKey"><inheritdoc cref="IDictionary{TKey, TValue}"
+    /// path="/typeparam[@name='TKey']"/></typeparam>
+    /// <typeparam name="TValue"><inheritdoc cref="IDictionary{TKey, TValue}"
+    /// path="/typeparam[@name='TValue']"/></typeparam>
+    /// <typeparam name="TResult">The return type of
+    /// <paramref name="processingAction"/>.</typeparam>
+    /// <param name="source">The <see cref="IDictionary{TKey, TValue}"/> to
+    /// remove element.</param>
+    /// <param name="key">
+    /// <inheritdoc cref="IDictionary{TKey, TValue}.Remove(TKey)"
+    /// path="/param[@name='key']"/></param>
+    /// <param name="processingAction">The action that will be executed with the
+    /// removed value as its argument.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="processingAction"/> is null.</exception>
+    public static T RemoveAndProcess<T, TKey, TValue, TResult>(this T source,
+      TKey key,
+      Func<TValue, TResult> processingAction)
+      where T : IDictionary<TKey, TValue>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (processingAction == null)
+        throw new ArgumentNullException("processingAction");
+      var element = source[key];
+      source.Remove(key);
+      processingAction(element);
+      return source;
+    }
+
+    /// <inheritdoc cref="RemoveAndProcess{T, TKey, TValue, TResult}(T, TKey, Func{TValue, TResult})"/>
+    public static IDictionary<TKey, TValue> RemoveAndProcess<TKey, TValue, TResult>(
+      this IDictionary<TKey, TValue> source,
+      TKey key,
+      Func<TValue, TResult> processingAction)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (processingAction == null)
+        throw new ArgumentNullException("processingAction");
+      var element = source[key];
+      source.Remove(key);
+      processingAction(element);
+      return source;
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+﻿# CLSS.ExtensionMethods.IDictionary.RemoveAndProcess
+
+### Problem
+
+It's common to find situations where removing an object from a collection means you also want to do something with it, such as disposing of it. In a barebone .NET application, you can count on the .NET Garbage Collector to clean up the removed object, but in many domains, disposing of an unused object requires an additional manual call, as the framework still keep references to it. These 2 operations are each done in a separate line of code and therefore not very friendly to a functional syntax.
+
+```csharp
+var NumberSpellings = new Dictionary<int, string>();
+Hide(NumberSpellings[4]);
+NumberSpellings.Remove(4);
+```
+
+### Solution
+
+`RemoveAndProcess` is an extension method for all `IDictionary<TKey, TValue>` types that combines these 2 steps into 1 method.
+
+```csharp
+using CLSS;
+
+var NumberSpellings = new Dictionary<int, string>();
+NumberSpellings.RemoveAndProcess(4, Hide);
+// using a lambda expression
+NumberSpellings.RemoveAndProcess(4, str => { PlayFadeAnimation(str); AddScore(str); });
+```
+
+`RemoveAndProcess` takes the value associated with the key specified by the first argument, removes the key from the source collection and passes that value to the delegate in the second argument.
+
+Non-void methods are also accepted as the second argument.
+
+```csharp
+using CLSS;
+
+bool TryDispose(ResourceHandler handler) { ... };
+
+var FileResourceMappings = new Dictionary<int, ResourceHandler>();
+FileResourceMappings.RemoveAndProcess(4, TryDispose);
+```
+
+`RemoveAndProcess` returns the source `IDictionary<TKey, TValue>` to be friendly to a functional-style call chain. The exact return type will be determined by the invocation syntax of `RemoveAndProcess`. With an implicit type invocation, it returns an `IDictionary<TKey, TValue>`. With an explicit type invocation, it returns the original collection type.
+
+```csharp
+using CLSS;
+
+var NumberSpellings = new Dictionary<int, string>();
+NumberSpellings.RemoveAndProcess(2, str => { ... }); // returns IDictionary<int, string>
+NumberSpellings.RemoveAndProcess<Dictionary<int, string>, int, string>(2,
+  str => { ... }); // returns Dictionary<int, string>
+```
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
